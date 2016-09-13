ea414654b2ae9deb58260444475af364e34fc6a2
Gemma Leigh <hello@gemmaleigh.com>
2016-09-13 17:22:22 +0100

    WIP, broken :(

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 'use strict'
 
 var gulp = require('gulp')
-var nodemon = require('gulp-nodemon')
-var browserSync = require('browser-sync').create()
-var reload = browserSync.reload
+// var nodemon = require('gulp-nodemon')
+// var browserSync = require('browser-sync').create()
+// var reload = browserSync.reload
 
 var gutil = require('gulp-util')
 var del = require('del')
@@ -21,121 +21,119 @@ gulp.task('clean', function (callback) {
 })
 
 // Create a govuk task to copy govuk dependencies to a govuk_modules folder
-// gulp.task('copy', ['clean'], function (cb) {
-//   // Copy govuk dependencies from node_modules to govuk_modules
-//   gulp.src(['node_modules/govuk_frontend_toolkit/**/*'])
-//     .pipe(gulp.dest('_gulp-test/govuk_modules/govuk_frontend_toolkit/'))
-//     .on('end', cb)
+gulp.task('copy', ['clean'], function (cb) {
+  // Copy govuk dependencies from node_modules to govuk_modules
+  gulp.src(['node_modules/govuk_frontend_toolkit/**/*'])
+    .pipe(gulp.dest('_gulp-test/govuk_modules/govuk_frontend_toolkit/'))
+    .on('end', cb)
 
-//   gulp.src(['node_modules/govuk_template_jinja/**/*'])
-//     .pipe(gulp.dest('_gulp-test/govuk_modules/govuk_template/'))
+  gulp.src(['node_modules/govuk_template_jinja/**/*'])
+    .pipe(gulp.dest('_gulp-test/govuk_modules/govuk_template/'))
 
-//   // Copy the govuk_template layout file to /lib
-//   gulp.src(['node_modules/govuk_template_jinja/views/layouts/*'])
-//     .pipe(gulp.dest('_gulp-test/lib/'))
+  // Copy the govuk_template layout file to /lib
+  gulp.src(['node_modules/govuk_template_jinja/views/layouts/*'])
+    .pipe(gulp.dest('_gulp-test/lib/'))
 
-//   gutil.log('copy govuk assets - task complete')
-// })
+  gutil.log('copy govuk assets - task complete')
+})
 
 // Encode HTML snippets
-// gulp.task('encode', function () {
-//   // Move snippets to a test folder
-//   gulp.src(['app/views/snippets/**/*'])
-//     .pipe(gulp.dest('_gulp-test/app/views/snippets/'))
+gulp.task('encode', function () {
+  // Move snippets to a test folder
+  gulp.src(['app/views/snippets/**/*'])
+    .pipe(gulp.dest('_gulp-test/app/views/snippets/'))
 
-//   gutil.log('copy test snippets - task complete')
+  gutil.log('copy test snippets - task complete')
 
-//   return gulp.src('./_gulp-test/app/views/snippets/*')
-//     .pipe(entities('encode'))
-//     .pipe(gulp.dest('./_gulp-test/app/views/snippets/encoded/'))
-// })
+  return gulp.src('./_gulp-test/app/views/snippets/*')
+    .pipe(entities('encode'))
+    .pipe(gulp.dest('./_gulp-test/app/views/snippets/encoded/'))
+})
 
 // Compile scss from public/sass to public/stylesheets
 gulp.task('styles', function () {
-  return gulp.src('/public/sass/**/*.scss')
+  return gulp.src('./public/sass/**/*.scss')
     .pipe(sass({
       outputStyle: 'expanded',
       includePaths: [
-        '/govuk_modules/govuk_template/assets/stylesheets',
-        '/govuk_modules/govuk_frontend_toolkit/stylesheets'
-      ]
+        '/node_modules/govuk_template/assets/stylesheets',
+        '/node_modules/govuk_frontend_toolkit/stylesheets']
     }).on('error', sass.logError))
-    .pipe(gulp.dest('/public/stylesheets/'))
-    .pipe(browserSync.stream())
+    .pipe(gulp.dest('./public/stylesheets/'))
+// .pipe(browserSync.stream())
 })
 
-// gulp.task('scss-lint', function () {
-//   return gulp.src('./public/sass/elements/**/*.scss')
-//     .pipe(scsslint({
-//       'bundleExec': true,
-//       'config': '.scss-lint.yml'
-//     }))
-// })
+gulp.task('scss-lint', function () {
+  return gulp.src('./public/sass/elements/**/*.scss')
+    .pipe(scsslint({
+      'bundleExec': true,
+      'config': '.scss-lint.yml'
+    }))
+})
 
-// gulp.task('standard', function () {
-//   return gulp.src([
-//     './public/javascripts/*.js'
-//   ])
-//     .pipe(standard())
-//     .pipe(standard.reporter('default', {
-//       breakOnError: true,
-//       quiet: true
-//     }))
-// })
+gulp.task('standard', function () {
+  return gulp.src([
+    './public/javascripts/*.js'
+  ])
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
+})
 
 // Build - run the govuk copy task, compile scss, encode HTML snippets, lint scss and js
-// gulp.task('build', function (callback) {
-//   runsequence('copy', ['styles', 'encode', 'lint'], callback)
-// })
+gulp.task('build', function (callback) {
+  runsequence('copy', ['styles', 'encode', 'lint'], callback)
+})
 
 // Lint - lint scss and js
-// gulp.task('lint', function (callback) {
-//   runsequence(['scss-lint', 'standard'], callback)
+gulp.task('lint', function (callback) {
+  runsequence(['scss-lint', 'standard'], callback)
+})
+
+// gulp.task('default', ['browser-sync', 'sass:watch'])
+
+// gulp.task('browser-sync', ['nodemon'],
+//   function () {
+//     browserSync.init(null, {
+//       proxy: 'http://localhost:3000',
+//       files: ['public/**/*.*'],
+//       browser: 'google chrome',
+//       port: 7000
+//     })
+//   }
+// )
+
+// gulp.task('nodemon',
+//   [],
+//   function (done) {
+//     var running = false
+
+//     return nodemon({
+//       script: 'server.js'
+//     })
+//       .on('start', function () {
+//         if (!running) {
+//           done()
+//         }
+//         running = true
+//       })
+//       .on('restart', function () {
+//         setTimeout(function () {
+//           reload()
+//         }, 500)
+//       })
+//   }
+// )
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('.public/sass/**/*.scss', ['styles'])
 // })
 
-gulp.task('serve',
-  [
-    'browser-sync'
-  ],
-  function () {
-    gulp.watch('public/*.js').on('change', reload)
-    gulp.watch('public/*.css').on('change', reload)
-    gulp.watch('app/views/*.html').on('change', reload)
-  }
-)
-
-gulp.task('browser-sync'),
-  [
-    'nodemon'
-  ],
-  function () {
-    browserSync.init(null, {
-      proxy: 'http://localhost',
-      browser: 'google chrome',
-      port: 3000
-    }
-  }
-)
-
-gulp.task('nodemon'),
-  [],
-  function (done) {
-    var running = false
-
-    return nodemon({
-      script: 'server.js',
-      watch: 'server.js'
-    })
-    .on('start', function () {
-      if (!running) {
-        done()
-      }
-      running = true
-    })
-    .on('restart', function () {
-      setTimeout(function () {
-        reload()
-      }, 500)
-    })
-  }
-)
+// gulp.task('watch',
+//   function () {
+//     gulp.watch('public/**/*.js').on('change', reload)
+//     gulp.watch('app/views/**/*.html').on('change', reload)
+//   }
+// )
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var path = require('path')
 var express = require('express')
 var nunjucks = require('express-nunjucks')
