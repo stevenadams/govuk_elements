a61f6a45aa15e8cc9eea7c403e2584971e10da73
Gemma Leigh <hello@gemmaleigh.com>
2016-09-13 13:35:39 +0100

    Add run sequence to run tasks in order

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,43 @@ var del = require('del')
 var sass = require('gulp-sass')
 var scsslint = require('gulp-scss-lint')
 var entities = require('gulp-html-entities')
+var runsequence = require('run-sequence')
 var standard = require('gulp-standard')
 
-// Create a govuk task to copy govuk dependencies to a govuk_modules folder
+// Delete the _gulp-test folder
+gulp.task('clean', function (callback) {
+  return del(['_gulp-test'])
+})
 
-gulp.task('govuk', function () {
+// Create a govuk task to copy govuk dependencies to a govuk_modules folder
+gulp.task('copy', ['clean'], function (cb) {
   // Copy govuk dependencies from node_modules to govuk_modules
-  gulp.src(['node_modules/govuk_frontend_toolkit/**/*']).pipe(gulp.dest('_gulp-test/govuk_modules/govuk_frontend_toolkit/'))
-  gulp.src(['node_modules/govuk_template_jinja/**/*']).pipe(gulp.dest('_gulp-test/govuk_modules/govuk_template/'))
-  gutil.log('template and tookit copied to govuk_modules')
+  gulp.src(['node_modules/govuk_frontend_toolkit/**/*'])
+    .pipe(gulp.dest('_gulp-test/govuk_modules/govuk_frontend_toolkit/'))
+    .on('end', cb)
+
+  gulp.src(['node_modules/govuk_template_jinja/**/*'])
+    .pipe(gulp.dest('_gulp-test/govuk_modules/govuk_template/'))
 
   // Copy the govuk_template layout file to /lib
-  gulp.src(['node_modules/govuk_template_jinja/views/layouts/*']).pipe(gulp.dest('_gulp-test/lib/'))
-  gutil.log('govuk_template layout copied to lib')
+  gulp.src(['node_modules/govuk_template_jinja/views/layouts/*'])
+    .pipe(gulp.dest('_gulp-test/lib/'))
 
-  // Move snippets to a test folder
-  gulp.src(['app/views/snippets/**/*']).pipe(gulp.dest('_gulp-test/app/views/snippets/'))
-  gutil.log('snippets duplicated in test folder')
+  gutil.log('copy task complete')
 })
 
 // Encode HTML snippets
-
 gulp.task('encode', function () {
+  // Move snippets to a test folder
+  gulp.src(['app/views/snippets/**/*'])
+    .pipe(gulp.dest('_gulp-test/app/views/snippets/'))
+
   return gulp.src('./_gulp-test/app/views/snippets/*')
     .pipe(entities('encode'))
     .pipe(gulp.dest('./_gulp-test/app/views/snippets/encoded/'))
 })
 
 // Compile scss from public/sass to public/stylesheets
-
 gulp.task('styles', function () {
   return gulp.src('./public/sass/**/*.scss')
     .pipe(sass({
@@ -45,7 +53,7 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('_gulp-test/public/stylesheets'))
 })
 
-gulp.task('styles-lint', function () {
+gulp.task('scss-lint', function () {
   return gulp.src('./public/sass/elements/**/*.scss')
     .pipe(scsslint({
       'bundleExec': true,
@@ -64,14 +72,17 @@ gulp.task('standard', function () {
     }))
 })
 
-// Delete the _gulp-test directory (change this to /public/stylesheets once working)
-
-gulp.task('clean', function (callback) {
-  return del(['_gulp-test'])
+// Build - run the govuk copy task, compile scss, encode HTML snippets, lint scss and js
+gulp.task('build', function (callback) {
+  runsequence('copy', ['styles', 'encode', 'lint'], callback)
 })
 
-// Also delete the encoded snippets folder
+// Lint - lint scss and js
+gulp.task('lint', function (callback) {
+  runsequence(['scss-lint', 'standard'], callback)
+})
 
-gulp.task('clean_snippets', function (callback) {
-  return del(['_gulp-test/app/views/snippets/encoded'])
+// Test - run the build task
+gulp.task('test', function (callback) {
+  runsequence(['build'], callback)
 })
