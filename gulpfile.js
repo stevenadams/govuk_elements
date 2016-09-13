d6d3ec295a230e0611f8976e75dda4d85d1b99c9
Gemma Leigh <hello@gemmaleigh.com>
2016-09-13 11:24:18 +0100

    Copy govuk dependencies from node_modules to govuk_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,24 @@
-var gulp = require('gulp')
+'use strict'
 
-gulp.task('setup', function() {
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var del = require('del')
+var sass = require('gulp-sass')
+var entities = require('gulp-html-entities')
+
+
+// Create a govuk task to copy govuk dependencies to a govuk_modules folder
+
+gulp.task('govuk', function() {
 
   // Copy govuk dependencies from node_modules to govuk_modules
-  gulp.src(['node_modules/govuk_frontend_toolkit/']).pipe(gulp.dest('govuk_modules/govuk_frontend_toolkit/'))
-  gulp.src(['node_modules/govuk_template_jinja/']).pipe(gulp.dest('govuk_modules/govuk_template/'))
+  gulp.src(['node_modules/govuk_frontend_toolkit/**/*']).pipe(gulp.dest('_gulp-test/govuk_modules/govuk_frontend_toolkit/'))
+  gulp.src(['node_modules/govuk_template_jinja/**/*']).pipe(gulp.dest('_gulp-test/govuk_modules/govuk_template/'))
+  // gutil.log('template and tookit copied to govuk_modules');
 
   // Copy the govuk_template layout file to /lib
-  gulp.src(['node_modules/govuk_template_jinja/views/layouts/']).pipe(gulp.dest('lib/'))
-
-  // Copy assets to /public
-  gulp.src(['app/assets/**/*']).pipe(gulp.dest('public'))
-}
-
-// Compile scss fiiles to css
-gulp.task('styles', function() {
+  gulp.src(['node_modules/govuk_template_jinja/views/layouts/*']).pipe(gulp.dest('_gulp-test/lib/'))
+  // gutil.log('govuk_template layout copied to lib');
 
 })
 
-
-// Lint install scss
-
-// Encode HTML snippets to app/snippets/encoded
-
-// Use Browsersync to watch for changes and restart app
-
-// Default task
-// Lists out available tasks
-gulp.task('default', function () {
-  console.log('Your first task')
-})
